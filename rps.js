1bbb67c0d2667e71aa250a40616471b194511a66
Aston <aston.ross@hotmail.co.uk>
2023-10-17 16:58:53 +0100

    fixed input to being case insensitive

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let move = ["Rock", "Paper", "Scissors"]
+    let move = ["rock", "paper", "scissors"]
 
 
     min = Math.ceil(0);
@@ -17,40 +17,44 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
-        case "Rock":
-            if (computerSelection == "Rock")
+        case "rock":
+            if (computerSelection == "rock")
                 return "You chose Rock and the Computer chose Rock!Its a Draw!"
 
 
-            if (computerSelection == "Scissors")
-                return "Player Wins! Rock breaks Scissors!"
+            if (computerSelection == "scissors")
+                return "You Win! Rock breaks Scissors!"
 
 
-            if (computerSelection == "Paper")
+            if (computerSelection == "paper")
 
                 return "You Lose! Paper covers Rock!"
             break;
 
-        case "Scissors":
-            if (computerSelection == "Rock")
+        case "scissors":
+            if (computerSelection == "rock")
                 return "You Lose! Rock breaks Scissors!"
 
-            if (computerSelection == "Scissors")
+            if (computerSelection == "scissors")
                 return "You chose Scissors and the Computer chose Scissors! Its a Draw!"
 
-            if (computerSelection == "Paper")
+            if (computerSelection == "paper")
                 return "You Win! Scissors cuts Paper!"
             break;
 
-        case "Paper":
-            if (computerSelection == "Rock")
+        case "paper":
+            if (computerSelection == "rock")
                 return "You Win! Paper covers Scissors!"
 
-            if (computerSelection == "Scissors")
+            if (computerSelection == "scissors")
                 return "You Lose! Scissors cuts Paper!"
 
-            if (computerSelection == "Paper")
+            if (computerSelection == "paper")
                 return "You chose Paper and the Computer chose Paper! Its a Draw!"
+            break;
+
+        default:
+            return "That weapon is not in the arsenal. Please choose either Rock, Paper or Scissors."
 
     }
 }
@@ -63,7 +67,7 @@ console.log(playRound(playerSelection, computerSelection))
 
 
 for (let loop = 0; loop < 5; loop++) {
-    const playerSelection = prompt("Make your choice!")
+    const playerSelection = prompt("Make your choice!").toLowerCase();
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection))
 
